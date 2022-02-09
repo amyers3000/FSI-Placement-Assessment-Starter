c52997e48e8ea6782ed4992ae6d2798abf508bbb
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-02-08 19:01:46 -0600

    Finished Assesment

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,68 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
 
-// We'll use these variables to track the counts of each cookie type
+let yourName = "Alex Myers" 
+
+
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
-// selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
 
-// Code to update name display
+
+
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+let tot = document.body.querySelector('#qty-total')
+let gquant = document.body.querySelector('#qty-gb')
+let ccquant = document.body.querySelector('#qty-cc')
+let sugarquant = document.body.querySelector('#qty-sugar')
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+const gbPlusBtn = document.querySelector('#add-gb')
+gbPlusBtn.addEventListener('click', function() {
+    gb++
+    gquant.textContent = `${gb}`
+    tot.textContent = `${gb + cc +sugar}`
+
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+const gbMinusBtn = document.body.querySelector('#minus-gb')
+gbMinusBtn.addEventListener('click', function(){
+    if(gb<=0){gb=0} else {
+        gb--
+        gquant.textContent = `${gb}`
+        tot.textContent = `${gb + cc +sugar}`
+    }
+})
+
+const ccPlusBtn = document.body.querySelector('#add-cc')
+ccPlusBtn.addEventListener('click', function(){
+    cc++
+    ccquant.textContent = `${cc}`
+    tot.textContent = `${gb + cc +sugar}`
+})
+
+const ccMinusBtn = document.body.querySelector('#minus-cc')
+ccMinusBtn.addEventListener('click', function(){
+    if(cc<=0){cc=0} else {
+        cc--
+        ccquant.textContent = `${cc}`
+        tot.textContent = `${gb + cc +sugar}`
+    }
+})
+
+const sugarPlusBtn = document.body.querySelector('#add-sugar')
+sugarPlusBtn.addEventListener('click', function(){
+    sugar++
+    sugarquant.textContent = `${sugar}`
+    tot.textContent = `${gb + cc +sugar}`
+})
+
+const sugarMinusBtn = document.body.querySelector('#minus-sugar')
+sugarMinusBtn.addEventListener('click', function(){
+    if(sugar<=0){sugar=0} else {
+        sugar--
+        sugarquant.textContent = `${sugar}`
+        tot.textContent = `${gb + cc +sugar}`
+    }
+})
+
